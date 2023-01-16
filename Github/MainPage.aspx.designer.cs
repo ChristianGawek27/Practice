72bd8411137698076c3d787abfdf9a885ccdda46
Christian Gawek <tun332920@temple.edu>
2023-01-16 16:43:10 -0500

    Added a new Label and css styling for the third label

--- Github/MainPage.aspx.designer.cs
+++ Github/MainPage.aspx.designer.cs
@@ -49,5 +49,14 @@ namespace Github
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label SecondLabel;
+
+        /// <summary>
+        /// ThirdLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ThirdLabel;
     }
 }
